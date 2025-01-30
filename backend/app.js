513ea9e56fb88c290574734a7d0b2d091c85031c
HruthikKK <hkk23052004@gmail.com>
2025-01-30 15:27:53 +0530

    check2

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import bodyParser from "body-parser"
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'https://intx.vercel.app' }));
 
 //Midlewares
 app.use(express.json({limit: "16kb"}))
